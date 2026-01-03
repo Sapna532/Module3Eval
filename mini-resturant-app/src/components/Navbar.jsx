6161f949cb0265e2ee19f90b6d0fe1f9c9f5740a
Sapna532 <sapnaydv37@gmail.com>
2026-01-03 20:30:04 +0530

    Navbar done

--- mini-resturant-app/src/components/Navbar.jsx
+++ mini-resturant-app/src/components/Navbar.jsx
@@ -16,6 +16,19 @@ setParkingFilter,
      return(
         <div style={{marginBottom: "20px"}}>
          <input ref={searchRef} placeholder="Serach by name" value={search} onChange={(e)=> setSearch(e.target.value)}/>
+         <select value={typeFilter} onChange={(e)=>setTypeFilter(e.target.value="")}> <option>All Types</option>
+         <option>Rajasthani</option>
+                <option>Gujarati</option>
+                <option>Jain</option>
+                <option>Thai</option>
+                <option>Mughalai </option>
+                <option>North</option>
+                <option>South</option>
+         
+         </select>
+         <select value={typeFilter} onChange={(e)=>setTypeFilter(e.target.value)}> <option value=""> All Parking</option>
+         <option  value="true"> Parking Availabe</option>
+         <option  value="false">No parking</option> </select>
         </div>
-     )
+     );
  }
